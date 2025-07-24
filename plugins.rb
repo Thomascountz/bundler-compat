3b4d0f4b15f1d4526120f6bb00cf747efd65bae5
Thomas Countz <thomascountz@gmail.com>
2025-07-24 11:29:01 +0200

    WIP

--- plugins.rb
+++ plugins.rb
@@ -7,19 +7,20 @@ class BundlerCompatPlugin < Bundler::Plugin::API
 
   def exec(command, args)
     target_version = args.first || "8.1.0"
-    
+
     begin
       conflict_finder = Bundler::Compat::ConflictFinder.new(target_version: target_version)
       results = conflict_finder.search
-      
+
       reporter = Bundler::Compat::Reporters::TextReporter.new(results)
       reporter.print
-      
+
       if results.conflicts.any?
         exit 1
       end
     rescue => e
       Bundler.ui.error "Error running compat check: #{e.message}"
+      e.backtrace.each { |line| Bundler.ui.error line }
       exit 1
     end
   end
